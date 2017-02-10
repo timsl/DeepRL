696e513dbdd3d146925f2bf39e345f8aa0075f6f
timsl <tiol@kth.se>
2017-02-11 00:17:45 +0100

    Starter agent

--- first.py
+++ first.py
@@ -1,12 +1,11 @@
-import numpy as np
 import gym
-import universe
+import universe  # register the universe environments
 
 env = gym.make('flashgames.DuskDrive-v0')
-env.configure(remotes=1)
+env.configure(remotes=1)  # automatically creates a local docker container
 observation_n = env.reset()
 
 while True:
-    action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]
-    observation_n, reward_n, done_n, info = env.step(action_n)
-    env.render()
+  action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]  # your agent here
+  observation_n, reward_n, done_n, info = env.step(action_n)
+  #env.render()
