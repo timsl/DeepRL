fcf772f447d3b9afcf5bfb6c0f1b9c580eb65b23
timsl <tiol@kth.se>
2017-02-10 14:07:07 +0100

    Init universe

--- first.py
+++ first.py
@@ -1,2 +1,12 @@
 import numpy as np
+import gym
+import universe
 
+env = gym.make('flashgames.DuskDrive-v0')
+env.configure(remotes=1)
+observation_n = env.reset()
+
+while True:
+    action_n = [[('KeyEvent', 'ArrowUp', True)] for ob in observation_n]
+    observation_n, reward_n, done_n, info = env.step(action_n)
+    env.render()
