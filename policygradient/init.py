7d7a53dfb2cdea6f3d898af8c173b8faa6033f30
timsl <tiol@kth.se>
2017-04-24 16:08:26 +0200

    init

--- policygradient/init.py
+++ policygradient/init.py
@@ -68,4 +68,58 @@ reward_sum = 0
 episode_number = 0
 
 while True:
+    cur_x = preprocess(observation)
+    x = cur_x - prev_x if prev_x is not None else np.zeros(D)
+    prev_x = cur_x
+
+    aprob, h = policy_forward(x)
+    action = 2 if np.random.uniform() < aprob else 3
+
+    xs.append(x)
+    hs.append(h)
+    y = 1 if action == 2 else 0
+    dlogps.append(y - aprob)
+
+    env.render()
+    observation, reward, done, info = env.step(action)
+    reward_sum += reward
+
+    drs.append(reward)
+
+    if done:
+        episode_number += 1
+
+    epx = np.vstack(xs) 
+    eph = np.vstack(hs) 
+    epdlogp = np.vstack(dlogps) 
+    epr = np.vstack(drs) 
+    xs,hs,dlogps,drs = [],[],[],[] 
+
     
+   
+    discounted_epr = discount_rewards(epr)
+  
+    discounted_epr -= np.mean(discounted_epr)
+    discounted_epr /= np.std(discounted_epr)
+
+ 
+    epdlogp *= discounted_epr 
+    grad = policy_backward(eph, epdlogp)
+    for k in model: grad_buffer[k] += grad[k] 
+
+   
+    
+    if episode_number % batch_size == 0:
+      for k,v in model.iteritems():
+        g = grad_buffer[k] 
+        rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
+        model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
+        grad_buffer[k] = np.zeros_like(v) 
+
+    
+    running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
+    print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
+    if episode_number % 100 == 0: pickle.dump(model, open('save.p', 'wb'))
+    reward_sum = 0
+    observation = env.reset() 
+    prev_x = None
